8933f5bd8884ae652fef38d22bd65ce015aae203
Pablo <209974936+adegadelivery@users.noreply.github.com>
2026-09-14 21:50:58 -0300

    fix: update home message (pessoa B)

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # Rota principal de boas-vindas
-    return "Hello, CI/CD!"
+    # Rota principal com mensagem da Pessoa B
+    return "Mensagem da Pessoa B"
 
 
 @app.route("/status")
