ddce55cee3d5ffef5a9318aede136118bf6623c1
Pablo <209974936+adegadelivery@users.noreply.github.com>
2026-09-14 21:46:58 -0300

    feat: add status endpoint

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
     return "Hello, CI/CD!"
 
 
+@app.route("/status")
+def status():
+    return {"status": "ok", "service": "checkpoint-cicd"}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
